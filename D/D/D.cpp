f16926e48713b1e1d2314bb70f303dadc70cc820
PXT <conan1069@qq.com>
2020-09-28 11:20:10 +0800

    提交除法代码

---
+++ D/D/D.cpp
@@ -0,0 +1,42 @@
+﻿#include "D.h"
+#include "../../A/A/A.h"
+#include "../../B/B/B.h" //隐式调用 需要将相关.dll文件拖至此exe文件下
+#include "../../C/C/C.h" 
+#include <Windows.h> //LoadLibrary是Windows.h库里的
+#include <stdio.h>  
+typedef int (*PMUL)(int a, int b); //定义指针指向mul
+
+int main(){
+
+	printf("请输入两个整数：");
+	int a, b;
+	scanf("%d%d", &a, &b);
+
+	int sum = add(a, b);
+	int cha = sub(a, b);
+	double shang = chu(a, b);
+
+	//加载乘法.dll文件  动态加载
+	//L:表示使用宽字符串  
+	HMODULE hDll = LoadLibrary(L"../../C/Debug/C.dll");
+	if (hDll == NULL){
+		printf("加载乘法DLL文件失败。\n");
+		return 0;
+	}
+	//获取模块中函数地址（指针）  返回一个函数指针
+	PMUL pMul = (PMUL)GetProcAddress(hDll, "mul");
+
+	//调用 乘法 接口
+	int ji = pMul(a, b);
+	
+	printf("%d + %d = %d\n", a, b, sum);
+	printf("%d - %d = %d\n", a, b, cha);
+	printf("%d * %d = %d\n", a, b, ji);
+	printf("%d / %d = %lf\n", a, b, shang);
+
+	//释放库
+	FreeLibrary(hDll);
+
+	system("pause");
+	return 0;
+}
---
+++ D/D/D.h
@@ -0,0 +1,16 @@
+﻿#ifndef D_H
+#define D_H
+
+
+double chu(double a, double b);
+
+
+double chu(double a, double b){
+	//以防除数和被除数为0
+	if (a==0 || b == 0){
+		return 0.0;
+	}
+	return a / b;
+}
+
+#endif
